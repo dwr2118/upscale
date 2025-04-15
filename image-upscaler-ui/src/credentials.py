6a70924ffdcd195081a9f73e9fa42d8344ea31a5
dwr2118 <dwr2118@columbia.edu>
2025-04-15 15:50:27 -0400

    template credentials file

---
+++ image-upscaler-ui/src/credentials.py
@@ -0,0 +1,3 @@
+SSH_HOST = None # insert the ec2 IP address here
+SSH_USER = None # insert the username you use to SSH into the ec2 instance 
+SSH_KEY_PATH = None # insert the absolute path to your .pem key
