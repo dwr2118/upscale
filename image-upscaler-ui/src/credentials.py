a7af16f64316bd431e25df950f9c429c00df7588
Diego Rivas Lazala <dwr2118@columbia.edu>
2025-05-08 23:26:27 +0000

    blank credentials added

---
+++ image-upscaler-ui/src/credentials.py
@@ -0,0 +1,3 @@
+SSH_HOST = "" # Replace with the EC2 instance public IP address
+SSH_USER = "" # Replace with the EC2 instance username
+SSH_KEY_PATH = "" # Replace with the path to your SSH key file
